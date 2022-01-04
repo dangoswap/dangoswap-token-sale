fb53def319a6bd8802622f7dcf3377469e94c61c
Tora <toramisu.c@gmail.com>
2022-01-04 20:42:44 +0700

    feat: เพิ่ม setPrice onlyOwner

--- contracts/TokenVendor.sol
+++ contracts/TokenVendor.sol
@@ -4,38 +4,76 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TokenVendor {
-
+contract TokenVendor is Ownable {
     using SafeMath for uint256;
 
     // token price for ETH
-    uint256 public tokensOutPerInNumerator = 12;
-    uint256 public tokensOutPerInDenominator = 100;
-    ERC20 public tokenIn;
-    ERC20 public tokenOut;
+    uint256 public tokensOutPerInNumerator;
+    uint256 public tokensOutPerInDenominator;
+    ERC20 public immutable tokenIn;
+    ERC20 public immutable tokenOut;
+    ERC20 public immutable weth;
     address public tokenInRecipient;
 
-    event SwapTokens(address indexed _from, address indexed _to, uint tokenInAmount, uint tokenOutAmount);
+    event SwapTokens(
+        address indexed _from,
+        address indexed _to,
+        uint256 tokenInAmount,
+        uint256 tokenOutAmount
+    );
 
-    constructor(ERC20 _tokenIn, ERC20 _tokenOut) {
+    constructor(
+        ERC20 _weth,
+        ERC20 _tokenIn,
+        ERC20 _tokenOut,
+        uint256 _tokensOutPerInNumerator,
+        uint256 _tokensOutPerInDenominator
+    ) {
         tokenIn = _tokenIn;
         tokenOut = _tokenOut;
+        weth = _weth;
+        setPrice(_tokensOutPerInNumerator, _tokensOutPerInDenominator);
     }
-    
-    function swapTokens(uint256 tokenInAmount, address from, address to) public returns (uint256 tokenOutAmount) {
-        uint256 amountToBuy = tokenInAmount
-            .mul(tokensOutPerInNumerator)
-            .div(tokensOutPerInDenominator);
+
+    function setPrice(
+        uint256 _tokensOutPerInNumerator,
+        uint256 _tokensOutPerInDenominator
+    ) public onlyOwner {
+        require(
+            _tokensOutPerInNumerator > 0,
+            "_tokensOutPerInNumerator must be positive"
+        );
+        require(
+            _tokensOutPerInDenominator > 0,
+            "_tokensOutPerInDenominator must be positive"
+        );
+        tokensOutPerInNumerator = _tokensOutPerInNumerator;
+        tokensOutPerInDenominator = _tokensOutPerInDenominator;
+    }
+
+    function swapTokens(
+        uint256 tokenInAmount,
+        address from,
+        address to
+    ) public returns (uint256 tokenOutAmount) {
+        uint256 amountToBuy = tokenInAmount.mul(tokensOutPerInNumerator).div(
+            tokensOutPerInDenominator
+        );
 
         // check if the Vendor Contract has enough amount of tokens for the transaction
         uint256 vendorBalance = tokenOut.balanceOf(address(this));
         require(
             vendorBalance >= amountToBuy,
-            "Vendor contract has not enough tokens in its balance"
+            "There is not enough tokens to swap out"
         );
 
-        bool received = tokenIn.transferFrom(from, tokenInRecipient, tokenInAmount);
+        bool received = tokenIn.transferFrom(
+            from,
+            tokenInRecipient,
+            tokenInAmount
+        );
         require(received, "Failed to receive token from user");
 
         // Transfer token to the msg.sender
