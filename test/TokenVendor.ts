9d7adacc29c894a06f78510ad1d8232de0587f17
Tora <toramisu.c@gmail.com>
2022-01-07 22:32:33 +0700

    test: เพิ่ม test เกี่ยวกับราคา กับการ swap เกิน

--- test/TokenVendor.ts
+++ test/TokenVendor.ts
@@ -18,22 +18,23 @@ describe("TokenVendor contract", function () {
   let carol: SignerWithAddress;
   let dev: SignerWithAddress;
   let minter: SignerWithAddress;
+  let owner: SignerWithAddress;
 
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
+    [minter, alice, bob, carol, dev, owner] = await ethers.getSigners();
     let TokenIn = await ethers.getContractFactory("MockERC20");
     let TokenOut = await ethers.getContractFactory("MockERC20");
-    let TokenVendor = await ethers.getContractFactory("TokenVendor");
-    [minter, alice, bob, carol, dev] = await ethers.getSigners();
+    let TokenVendor = await ethers.getContractFactory("TokenVendor", owner);
 
-
-    tokenIn = <ERC20> await TokenIn.deploy('tokenIn Token', 'TI', 2000);
+    tokenIn = <ERC20> await TokenIn.deploy('tokenIn Token', 'TI', 20000000);
     tokenOut = <ERC20> await TokenOut.deploy('tokenOut Token', 'TO', 1000);
     tokenVendor = <TokenVendor> await TokenVendor.deploy(tokenIn.address, tokenIn.address, tokenOut.address, dev.address, 12, 100);
 
+
     await tokenIn.connect(minter).transfer(alice.address, 1000)
     await tokenIn.connect(minter).transfer(bob.address, 1000)
-    await tokenOut.connect(minter).transfer(tokenVendor.address, 1000)
+    await tokenOut.connect(minter).transfer(tokenVendor.address, 1000)    
 
     //console.log('balance tokenVendor for tokenOut token: ', (await tokenOut.balanceOf(tokenVendor.address)).toString());
   });
@@ -80,13 +81,32 @@ describe("TokenVendor contract", function () {
       await expect(p).to.be.rejectedWith(Error);
     });
 
-    // it("Should not be able to swap with not enough tokenOut in contract wallet") {
+    it("Should not be able to swap with not enough tokenOut in contract wallet", async () => {
+      await tokenIn.connect(minter).transfer(alice.address, 100000)
+      let p = tokenVendor.connect(alice).swapTokens(100000, alice.address, alice.address);
+      await expect(p).to.be.rejectedWith(Error);
+    });
 
-    // };
+    it("Should not be able to change price if not owner", async () => {
+      let p = tokenVendor.connect(alice).setPrice(12, 100);
+      await expect(p).to.be.rejectedWith(Error);      
+    });
 
-    // it("Should not be able to change price if not owner") {
-      
-    // };
+    it("Should be able to change price if is owner", async () => {
+      var t = await tokenVendor.setPrice(11, 99);
+      expect(await tokenVendor.tokensOutPerInNumerator()).to.be.eq(11);
+      expect(await tokenVendor.tokensOutPerInDenominator()).to.be.eq(99);
+    });
+
+    it("Should not be able to change price numerator to zero", async () => {
+      let p = tokenVendor.connect(owner).setPrice(0, 1);      
+      await expect(p).to.be.rejectedWith(Error);      
+    });
+
+    it("Should not be able to change price denominator to zero", async () => {
+      let p = tokenVendor.connect(owner).setPrice(1, 0);
+      await expect(p).to.be.rejectedWith(Error);      
+    });
 
   });
 
