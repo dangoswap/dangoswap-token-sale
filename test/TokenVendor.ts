b30169bb050065e7c43d3022d904f058d55381c8
Tora <toramisu.c@gmail.com>
2022-01-07 21:00:49 +0700

    feat: เพิ่ม test token in มีมากกว่าที่ user มี และปรับ dependencies เกี่ยวกับ test ไปไว้ใน devDependencies หมด

--- test/TokenVendor.ts
+++ test/TokenVendor.ts
@@ -1,19 +1,22 @@
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect } from "chai";
+import { expect, use as chaiUse } from "chai";
 import { Contract } from "ethers";
 import { ethers } from "hardhat";
 
+import chaiAsPromised from "chai-as-promised";
+chaiUse(chaiAsPromised);
+
 describe("TokenVendor contract", function () {
 
-  let tokenIn : Contract;
-  let tokenOut : Contract;
-  let tokenVendor : Contract;
-  let alice : SignerWithAddress;
-  let bob : SignerWithAddress;
-  let carol : SignerWithAddress;
-  let dev : SignerWithAddress;
-  let minter : SignerWithAddress;
+  let tokenIn: Contract;
+  let tokenOut: Contract;
+  let tokenVendor: Contract;
+  let alice: SignerWithAddress;
+  let bob: SignerWithAddress;
+  let carol: SignerWithAddress;
+  let dev: SignerWithAddress;
+  let minter: SignerWithAddress;
 
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
@@ -77,18 +80,7 @@ describe("TokenVendor contract", function () {
 
       // Swap 100 tokenIn from alice to tokenOut.
       let p = tokenVendor.connect(alice).swapTokens(100, alice.address, alice.address);
-      await expect(p)
-
-      // Check balances.
-      const finalAliceBalance = await tokenIn.balanceOf(alice.address);
-      expect(finalAliceBalance).to.equal(initialAliceBalance - 100);
-
-      const finalAliceTokenOutBalance = await tokenOut.balanceOf(alice.address);
-      expect(finalAliceTokenOutBalance).to.equal(12);
-
-      const devBalance = await tokenIn.balanceOf(dev.address);
-      expect(devBalance).to.equal(100);
-
+      await expect(p).to.be.rejectedWith(Error);
     });
 
   });
