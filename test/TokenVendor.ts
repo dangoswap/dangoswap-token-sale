822cac6c2ae4fd91056398e45229b47494b80d82
Tora <toramisu.c@gmail.com>
2022-01-11 18:10:51 +0700

    feat: ใช้ BigNumber ตอน test

--- test/TokenVendor.ts
+++ test/TokenVendor.ts
@@ -1,12 +1,11 @@
 
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { expect, use as chaiUse } from "chai";
 import { ethers } from "hardhat";
+import { expect, use as chaiUse } from "chai";
 import { ERC20, TokenVendor } from "../typechain-types";
+import { BigNumber } from "ethers";
 
-
-import chaiAsPromised from "chai-as-promised";
-chaiUse(chaiAsPromised);
+chaiUse(require('chai-as-promised'));
 chaiUse(require('chai-string'));
 
 describe("TokenVendor contract", function () {
@@ -24,15 +23,14 @@ describe("TokenVendor contract", function () {
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
     [minter, alice, bob, carol, dev, owner] = await ethers.getSigners();
-    let TokenIn = await ethers.getContractFactory("MockERC20");
-    let TokenOut = await ethers.getContractFactory("MockERC20");
-    let TokenVendor = await ethers.getContractFactory("TokenVendor", owner);
+    let erc20Factory = await ethers.getContractFactory("MockERC20");    
+    let vendorFactory = await ethers.getContractFactory("TokenVendor", owner);
 
-    tokenIn = <ERC20> await TokenIn.deploy('tokenIn Token', 'TI', 20000000);
-    tokenOut = <ERC20> await TokenOut.deploy('tokenOut Token', 'TO', 1000);
-    tokenVendor = <TokenVendor> await TokenVendor.deploy(tokenIn.address, tokenIn.address, tokenOut.address, dev.address, 12, 100);
-    tokenVendor = tokenVendor.connect(ethers.getDefaultProvider());
-
+    tokenIn = <ERC20> await (await erc20Factory.deploy('tokenIn Token', 'TI', 20000000)).connect(ethers.provider);
+    tokenOut = <ERC20> await (await erc20Factory.deploy('tokenOut Token', 'TO', 1000)).connect(ethers.provider);
+    tokenVendor = <TokenVendor> await (
+      await vendorFactory.deploy(tokenIn.address, tokenIn.address, tokenOut.address, dev.address, 12, 100)
+    ).connect(ethers.provider);
 
     await tokenIn.connect(minter).transfer(alice.address, 1000)
     await tokenIn.connect(minter).transfer(bob.address, 1000)
@@ -48,6 +46,11 @@ describe("TokenVendor contract", function () {
       expect(await tokenOut.totalSupply()).to.equal(tokenVendorBalance);
     });
 
+    it("Should be able to read contract without connecting", async function() {
+      const d = await tokenVendor.tokensOutPerInNumerator();
+      expect(d).to.equal(12);
+    });
+
     it("Should not be able to call TokenVendor without connecting", async function() {
       const p = tokenVendor.setPrice(1,1);
       expect(p).to.be.rejectedWith(Error);
@@ -55,19 +58,33 @@ describe("TokenVendor contract", function () {
 
   });
 
+  describe("BigNumber", function() {
+    it("1 should be equal to 1", async function() {
+      const one1 = BigNumber.from(1);
+      const one2 = BigNumber.from(1);
+      expect(one1).to.equal(one2);
+    });
+
+    it("1 should not be equal to 2", async function() {
+      const one = BigNumber.from(1);
+      const two = BigNumber.from(2);
+      expect(one).to.not.equal(two);
+    });
+  });
+
   describe("Transactions", function () {
 
     it("Should update balances after swap", async function () {
       const initialAliceBalance = await tokenIn.balanceOf(alice.address);
 
-      await tokenIn.connect(alice).approve(tokenVendor.address, '1000');
+      await tokenIn.connect(alice).approve(tokenVendor.address, 1000);
 
       // Swap 100 tokenIn from alice to tokenOut.
       await tokenVendor.connect(alice).swapTokens(100, alice.address, alice.address);
 
       // Check balances.
       const finalAliceBalance = await tokenIn.balanceOf(alice.address);
-      expect(finalAliceBalance).to.be.eq(initialAliceBalance.sub(100));
+      expect(finalAliceBalance).to.equal(initialAliceBalance.sub(100));
 
       const finalAliceTokenOutBalance = await tokenOut.balanceOf(alice.address);
       expect(finalAliceTokenOutBalance).to.equal(12);
@@ -104,8 +121,8 @@ describe("TokenVendor contract", function () {
 
     it("Should be able to change price if is owner", async () => {
       var t = await tokenVendor.connect(owner).setPrice(11, 99);
-      expect(await tokenVendor.connect(alice).tokensOutPerInNumerator()).to.be.eq(11);
-      expect(await tokenVendor.connect(alice).tokensOutPerInDenominator()).to.be.eq(99);
+      expect(await tokenVendor.tokensOutPerInNumerator()).to.be.eq(11);
+      expect(await tokenVendor.tokensOutPerInDenominator()).to.be.eq(99);
     });
 
     it("Should not be able to change price numerator to zero", async () => {
