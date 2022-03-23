5dd8e196319b933efe3272b8d8f6ca3faa6e6e65
Tora <toramisu.c@gmail.com>
2022-03-23 22:10:23 +0700

    fix: incorrect syntax

--- test/Token.js
+++ test/Token.js
@@ -30,8 +30,8 @@ describe('Token contract', function () {
   // time. It receives a callback, which can be async.
   beforeEach(async function () {
     // Get the ContractFactory and Signers here.
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners()
     Token = await ethers.getContractFactory('Token')
-    let [owner, addr1, addr2, ...addrs] = await ethers.getSigners()
 
     // To deploy our contract, we just have to call Token.deploy() and await
     // for it to be deployed(), which happens once its transaction has been
